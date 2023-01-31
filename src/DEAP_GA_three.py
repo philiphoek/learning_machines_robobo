4e816e6da96ad298fcd61ce7bdc7a8b30e0f7dea
philip van der hoek <pec.vdhoek@gmail.com>
2023-01-31 16:13:48 +0100

    Update DEAP_GA_three.py

--- src/DEAP_GA_three.py
+++ src/DEAP_GA_three.py
@@ -81,7 +81,7 @@ def simulation(controller, robot):
     food_delivered = 0
     initial_distance_food_to_base = controller.getDistance()
     steps_taken = 0
-    times_food_in_gripper = 0
+    time_steps_food_in_gripper = 0
     reward_food_in_gripper_and_seeing_base = 0
     object_hit = 0
     reward_having_food_in_center_of_image = 0
@@ -94,7 +94,7 @@ def simulation(controller, robot):
             break
 
         if controller.food_in_gripper == 1:
-            times_food_in_gripper += 0.25
+            time_steps_food_in_gripper += 1
 
         if controller.rob.check_for_collision():
             object_hit = 1
@@ -102,14 +102,9 @@ def simulation(controller, robot):
             print('Object is hit')
             break
 
-        if (controller.green_left == 1 or controller.green_right) and controller.food_in_gripper:
-            reward_food_in_gripper_and_seeing_base += 0.25
-
-        if controller.green_center == 1 and controller.food_in_gripper == 1:
-            reward_food_in_gripper_and_seeing_base += 0.5
-
-        if controller.red_center == 1:
-            reward_having_food_in_center_of_image += 0.25
+        if steps_taken > 50 and time_steps_food_in_gripper == 0:
+            print('Robot has not managed to get food in gripper for 50 time steps')
+            break
 
         steps_taken += 1
 
@@ -117,17 +112,13 @@ def simulation(controller, robot):
     relative_distance_food_to_base = final_distance_food_to_base / initial_distance_food_to_base  # closer to zero means food is close to the base
     print(f"Food deliverd: {food_delivered}")
     print(f"Relative distance travelled food to base: {relative_distance_food_to_base}")
-    print(f"Times food in gripper: {times_food_in_gripper}")
-    print(f"Reward for having food in center of image: {reward_having_food_in_center_of_image}")
-    print(f"Reward seeing base while having food in gripper: {reward_food_in_gripper_and_seeing_base}")
+    print(f"Time steps food in gripper: {time_steps_food_in_gripper}")
     print(f"Object hit: {object_hit}")
     fitness = 0
-    fitness += 200 * food_delivered
-    fitness += 50 * (1 - relative_distance_food_to_base)
-    fitness += (times_food_in_gripper * 1)
-    fitness += reward_having_food_in_center_of_image
-    fitness += reward_food_in_gripper_and_seeing_base
-    fitness -= 200 * object_hit
+    # fitness is highest when food is closest to base and the least amount of steps are taken
+    fitness += 200 * (1 - relative_distance_food_to_base) * (1 - (steps_taken/allowed_steps))
+    # make fitness zero when object is hit
+    fitness = fitness * (1 - object_hit)
     print(f"fitness: {fitness}")
 
     controller.rob.stop_world()
